ba46bf0c48edb870b6b64877b4167a52ca785a33
Charlie <cvuong90@gmail.com>
2022-06-29 22:03:39 -0700

    Remove source maps because of incorrect line number

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
+/******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
 /***/ 7351:
@@ -28712,4 +28712,3 @@ var CI = process.env.CI;
 module.exports = __webpack_exports__;
 /******/ })()
 ;
-//# sourceMappingURL=index.js.map
