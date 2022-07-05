b599671cacf13d632d956dfd4f11750ba682235c
Charlie <cvuong90@gmail.com>
2022-07-05 00:03:06 -0700

    Prepend the index.md path with GITHUB_WORKSPACE

--- scripts/buildDocs.sh
+++ scripts/buildDocs.sh
@@ -5,12 +5,12 @@
 yarn
 
 # Create a empty index.md file (if it does not already exist), so we can poll it
-if [ ! -f "./$DOCS_DIR/index.md" ]; then
-  touch "./$DOCS_DIR/index.md"
+if [ ! -f "${GITHUB_WORKSPACE}/$DOCS_DIR/index.md" ]; then
+  touch "${GITHUB_WORKSPACE}/$DOCS_DIR/index.md"
 fi
 
-# Generate the docs
-yarn next build
+# # Generate the docs
+# yarn next build
 
-# Generate static assets for the docs
-yarn next export
+# # Generate static assets for the docs
+# yarn next export
