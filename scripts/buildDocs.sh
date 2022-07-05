8519a8a233ef2c896ad3d19daa7eb187aa5b983b
Charlie <cvuong90@gmail.com>
2022-07-05 15:46:40 -0700

    Add debugging to buildDocs.sh

--- scripts/buildDocs.sh
+++ scripts/buildDocs.sh
@@ -16,8 +16,12 @@ yarn next build
 yarn next export
 
 # Rename generated out/_next folder to out/next because GitHub Pages
-# does not serve folders that begin with an _.
+# does not serve folders that begin with an _
 mv out/_next out/next
 
-# Find and replace all references of _next/ to next/ in all of the assets.
-find out/ -type f -name "*.html" -print0 | xargs -0 sed -i '' -e 's/_next/next/g'
+# Find and replace all references of _next/ to next/ in all of the assets
+echo "Add debugging statements"
+pwd
+ls -la
+find out -type f -name "*.html" -print0
+find out -type f -name "*.html" -print0 | xargs -0 sed -i '' -e 's/_next/next/g'
