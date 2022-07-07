41c790b083d0c32d44150a4cd47a80367111afb6
Charlie <cvuong90@gmail.com>
2022-07-06 20:10:06 -0700

    Debug jest path

--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -1,9 +1,10 @@
 # Run the following command to test locally
 #   GITHUB_WORKSPACE=$(pwd) DOCS_DIR=docs/ ./scripts/runTests.sh
 
+echo "docs dir"
+echo ${GITHUB_WORKSPACE}/$DOCS_DIR
 yarn jest ${GITHUB_WORKSPACE}/$DOCS_DIR
 echo "in between"
-yarn jest ${GITHUB_WORKSPACE}/$DOCS_DIR/dir/test
 
 # Used for debugging
 ls -la ${GITHUB_WORKSPACE}/$DOCS_DIR
