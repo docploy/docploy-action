8950f2cf9e811b07806e477a6ee63630403b2e4f
Charlie <cvuong90@gmail.com>
2022-07-06 18:08:11 -0700

    Add more debugging when running tests

---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  roots: ['/home/runner/work/test-action/test-action/docs/'],
+};
--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -1,7 +1,9 @@
 # Run the following command to test locally
 #   GITHUB_WORKSPACE=$(pwd) DOCS_DIR=docs/ ./scripts/runTests.sh
 
-yarn test ${GITHUB_WORKSPACE}/$DOCS_DIR
+yarn jest ${GITHUB_WORKSPACE}/$DOCS_DIR
+echo "in between"
+yarn jest ${GITHUB_WORKSPACE}/$DOCS_DIR/dir/test
 
 # Used for debugging
 ls -la ${GITHUB_WORKSPACE}/$DOCS_DIR
