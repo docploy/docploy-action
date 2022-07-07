c97c6c6b6634a8fa354fcae65ef4e0d1cba3383d
Charlie <cvuong90@gmail.com>
2022-07-06 17:29:28 -0700

    List out contents for debugging

--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -2,3 +2,7 @@
 #   GITHUB_WORKSPACE=$(pwd) DOCS_DIR=docs/ ./scripts/runTests.sh
 
 yarn test ${GITHUB_WORKSPACE}/$DOCS_DIR
+
+# Used for debugging
+ls -la ${GITHUB_WORKSPACE}/$DOCS_DIR
+ls -la ${GITHUB_WORKSPACE}/$DOCS_DIR/dir/test
