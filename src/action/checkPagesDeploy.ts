94704851cf424a8c3252b0555433d0417e2ce604
Charlie <cvuong90@gmail.com>
2022-07-21 16:04:06 -0700

    Add a comment to the pull request when the build is finished

--- src/action/checkPagesDeploy.ts
+++ src/action/checkPagesDeploy.ts
@@ -1,4 +1,5 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 
 import axios from 'axios';
 import { getDocsUrl } from 'src/utils/url';
@@ -6,6 +7,30 @@ import { getDocsUrl } from 'src/utils/url';
 const DEFAULTS = {
   TIMEOUT: 120, // 2 minutes is recommended because GitHub pages can take 1+ minute to deploy
 };
+
+// Send the deploy status as a comment to the pull request
+async function sendStatusMessage(status: 'pass' | 'fail', docsUrl?: string) {
+  const token = core.getInput('GITHUB_TOKEN');
+  const context = github.context;
+  let message;
+
+  if (status === 'pass') {
+    message = 'Your docs were successfuly deployed to: ' + docsUrl;
+  } else {
+    message =
+      'Your docs failed to deploy. Expand action output for more details';
+  }
+
+  const pullRequest = context.payload?.pull_request?.number;
+  if (pullRequest) {
+    const octokit = github.getOctokit(token);
+    await octokit.rest.issues.createComment({
+      ...context.repo,
+      issue_number: pullRequest,
+      body: message,
+    });
+  }
+}
 
 (async function () {
   const timeout = parseInt(core.getInput('timeout')) || DEFAULTS.TIMEOUT;
@@ -26,12 +51,14 @@ const DEFAULTS = {
 
     if (res && res.status === 200) {
       console.log('We successfully deployed the docs on:', deployedDocsUrl);
+      sendStatusMessage('pass', deployedDocsUrl);
       clearInterval(timer);
       return;
     }
 
     if (Date.now() > endTime) {
       console.error('We were unable to deploy the docs.');
+      sendStatusMessage('fail');
       process.exit(1);
     }
   }, 3000);
