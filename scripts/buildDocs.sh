31e8d5ee2296d403778728ea31aef6fd5ab74a4b
Charlie <cvuong90@gmail.com>
2022-07-22 11:30:20 -0700

    Remove renaming _next directory

--- scripts/buildDocs.sh
+++ scripts/buildDocs.sh
@@ -18,21 +18,9 @@ yarn next build
 # Generate static assets for the docs
 yarn next export
 
-# Rename generated out/_next folder to out/next because GitHub Pages
-# does not serve folders that begin with an _
-mv out/_next out/next
-
 # Move serve.json into the out/ directory to prevent the serve package from
 # redirecting paths with .html appended at the end.
 # (This is only used for development where we use the "serve" package to serve
 # static HTML files)
 # https://github.com/vercel/serve-handler#cleanurls-booleanarray
 cp serve.json out/serve.json
-
-# Find and replace all references of _next/ to next/ in all of the assets
-# Note, using sed -i '' will only work on Mac, so remove -i '' to support Ubuntu
-if [ `uname -s` = "Darwin" ]; then
-  find out -type f -name "*.html" -print0 | xargs -0 sed -i '' 's/_next/next/g'
-else
-  find out -type f -name "*.html" -print0 | xargs -0 sed -i 's/_next/next/g'
-fi
