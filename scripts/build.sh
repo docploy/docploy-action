84765812ff58f9a7a5d5e6dc19b5ad062377128c
Charlie <cvuong90@gmail.com>
2022-08-11 15:05:39 -0700

    Add comment in build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 # Clean any existing built files
 rm -rf dist/
 
-# Use ncc to build the script for detecting when GitHub Pages were deployed
+# Build a script to detect when GitHub Pages were deployed
 yarn run ncc build -s ./src/action/checkPagesDeploy.ts -o ./dist/checkPagesDeploy
