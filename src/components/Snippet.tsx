71d35ff7432a293e304c7f8b6d721482eb9282d7
Charlie Vuong <cvuong@users.noreply.github.com>
2022-08-12 10:34:27 -0700

    Add copy support (#9)
    * Add heroicons
    
    * Add support for copying code snippet

--- src/components/Snippet.tsx
+++ src/components/Snippet.tsx
@@ -1,6 +1,7 @@
 import 'prismjs/themes/prism-okaidia.css';
 import 'prismjs/components/prism-python';
 
+import { ClipboardCheckIcon, ClipboardIcon } from '@heroicons/react/outline';
 import { useEffect, useState } from 'react';
 
 import prismjs from 'prismjs';
@@ -17,13 +18,12 @@ type Props = {
 function Snippet({ snippets }: Props) {
   const languages = snippets.map((snippet) => snippet.language);
 
-  const [currentLanguage, setLanguage] = useState(languages[0]);
+  const [snippetIndex, setSnippetIndex] = useState(0);
   const [currentCode, setCode] = useState('');
+  const [hasCopied, setHasCopied] = useState(false);
 
   useEffect(() => {
-    const currentSnippet = snippets.find(
-      (snippet) => snippet.language === currentLanguage
-    );
+    const currentSnippet = snippets[snippetIndex];
     let code = '';
     if (currentSnippet) {
       const { content, language } = currentSnippet;
@@ -35,24 +35,32 @@ function Snippet({ snippets }: Props) {
       );
     }
     setCode(code);
-  }, [currentLanguage]);
+  }, [snippetIndex]);
   const buttonClass = 'cursor-pointer font-bold p-2 text-sm text-slate-200';
   const selectedButtonClass = buttonClass + ' bg-slate-600 rounded-md';
 
+  const onCopy = async () => {
+    await navigator.clipboard.writeText(snippets[snippetIndex].content);
+    setHasCopied(true);
+  };
+
+  const onLanguageClick = (i: number) => {
+    setSnippetIndex(i);
+    setHasCopied(false);
+  };
+
   return (
-    <div className="bg-slate-800 my-4 px-8 pb-8 pt-4 rounded-lg">
+    <div className="bg-slate-800 my-4 px-8 pb-8 pt-4 relative rounded-lg">
       <ul className="flex mb-6 gap-2 items-baseline">
-        {languages.map((language: string) => {
+        {languages.map((language: string, i: number) => {
           return (
             <li key={language}>
               <button
                 role="tab"
-                aria-selected={language === currentLanguage}
-                onClick={() => setLanguage(language)}
+                aria-selected={i === snippetIndex}
+                onClick={() => onLanguageClick(i)}
                 className={
-                  language === currentLanguage
-                    ? selectedButtonClass
-                    : buttonClass
+                  i === snippetIndex ? selectedButtonClass : buttonClass
                 }
               >
                 {language}
@@ -61,8 +69,19 @@ function Snippet({ snippets }: Props) {
           );
         })}
       </ul>
+      {hasCopied ? (
+        <ClipboardCheckIcon
+          onClick={onCopy}
+          className="absolute cursor-pointer text-green-400 h-6 w-6 top-4 right-4"
+        />
+      ) : (
+        <ClipboardIcon
+          onClick={onCopy}
+          className="absolute cursor-pointer text-slate-500 h-6 w-6 top-4 right-4"
+        />
+      )}
       <pre
-        className={`language-${currentLanguage} rounded-md !text-sm !bg-slate-800 !p-0`}
+        className={`language-${snippets[snippetIndex].language} rounded-md !text-sm !bg-slate-800 !p-0`}
         dangerouslySetInnerHTML={{ __html: currentCode }}
       ></pre>
     </div>
