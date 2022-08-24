57437e713bd92125b5858f603a722c015663cf88
Charlie <cvuong90@gmail.com>
2022-08-24 14:57:43 -0700

    Wrap env var using double quotes

--- scripts/sendAnalytics.sh
+++ scripts/sendAnalytics.sh
@@ -4,4 +4,4 @@ curl -i -X POST https://plausible.io/api/event \
   -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36 OPR/71.0.3770.284' \
   -H 'X-Forwarded-For: 127.0.0.1' \
   -H 'Content-Type: application/json' \
-  --data '{"name":"Run","url":"https://github.com","domain":"docploy.com","screen_width":1666,"props":{"owner":"$GITHUB_REPOSITORY"}}'
+  --data '{"name":"Run","url":"https://github.com","domain":"docploy.com","screen_width":1666,"props":{"owner":"'"$GITHUB_REPOSITORY"'"}}'
